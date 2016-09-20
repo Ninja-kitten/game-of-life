27faa6bbd40382a1a4e8d3e532f88e790c8f3f4d
Ninja-kitten <tran260@mail.chapman.edu>
2016-09-19 19:43:50 -0700

    Update GameofLife.cpp

--- GameofLife.cpp
+++ GameofLife.cpp
@@ -26,4 +26,56 @@ GameofLife::nextGen(){
       }
     }
   }
+  if(BoundaryMode::DOUGHNUT==boundary){
+    doughnutFill();
+  }
 }
+
+//fill the buffer zone for mirror mode
+GameofLife::mirrorFill(){
+for(int i = 0; i<oldBoard.getRows; ++i){
+  oldBoard.setCell(i,0,'X')
+  oldBoard.setCell(i,columns+1,'X')
+  newBoard.setCell(i,0,'X')
+  newBoard.setCell(i,columns+1,'X')
+}
+  for(int j = 0; j<oldBoard.getColumns;++j)
+  {
+    oldBoard.setCell(0,j,'X')
+    oldBoard.setCell(rows+1,j,'X')
+    newBoard.setCell(0,j,'X')
+    newBoard.setCell(rows+1,j,'X')
+  }
+}
+
+//fills the buffer zone for classic mode
+GameofLife::classicFill(){
+  for(int i = 0; i<2+oldBoard.getRows; ++i)
+  {
+    oldBoard.setCell(i,0,'-')
+    oldBoard.setCell(i,columns+1,'-')
+    newBoard.setCell(i,0,'-')
+    newBoard.setCell(i,columns+1,'-')
+  }
+for(int j = 0; j<2+oldBoard.getColumns;++j)
+  {
+    oldBoard.setCell(0,j,'-')
+    oldBoard.setCell(rows+1,j,'-')
+    newBoard.setCell(0,j,'-')
+    newBoard.setCell(rows+1,j,'-')
+  }
+}
+
+//fills the buffer zone for doughnut mode
+GameofLife::doughnutFill(){
+  for(int i=1; i<1+newBoard.getRows();++i)
+  {
+    newBoard.setcell(i,0,newBoard.getCell(i,columns))
+    newBoard.setcell(i,newBoard.getColumns(),newBoard.getCell(i,columns))
+  }
+    for(int j=1, j<1+newBoard.getColumns();++i)
+  {
+    newBoard.setcell(0,j,newBoard.getCell(i,columns))
+    newBoard.setcell(newBoard.getRows+1,j,newBoard.getCell(i,columns))
+  }
+}  
