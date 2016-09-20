0d85a152ed7dda0e995be36c435741bda15c8b43
Ninja-kitten <tran260@mail.chapman.edu>
2016-09-19 19:09:16 -0700

    Update GameofLife.cpp

--- GameofLife.cpp
+++ GameofLife.cpp
@@ -1,3 +1,29 @@
+#include "GameofLife.h"
 GameofLife::GameofLife(){
+}
 
+GameofLife::~GameofLife(){
+}
+
+GameofLife::nextGen(){
+  for(int i = 1; i<=oldBoard.getRows(); ++i){
+    for(int j = 1; j<=oldBoard.getColumns();++j){
+      if (oldBoard.countNeighbors(i,j)<=1)
+      {
+        newBoard.setCell(i,j,'-');
+      }
+      else if(oldBoard.countNeighbors(i,j)==3)
+      {
+        newBoard.setCell(i,j,'X');
+      }
+      else if (oldboard.countNeighbors(i,j)>=4)
+      {
+        newBoard.setCell(i,j,'-');
+      }
+      else
+      {
+        newBoard.setCell(i,j,oldBoard.getCell(i,j))
+      }
+    }
+  }
 }
