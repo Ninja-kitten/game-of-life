90a48a8e5e5d93c05af33fda48a6537ea8afe8f5
sakthikasthuri <kasth100@mail.chapman.edu>
2016-09-19 18:38:54 -0700

    Update GameBoard.h

--- GameBoard.h
+++ GameBoard.h
@@ -3,13 +3,13 @@ public:
 GameBoard(int,int,boundarymode);
 ~GameBoard();
 
-bool isNeighbor(Cell,Cell);
+/*bool isNeighbor(Cell,Cell);
 bool isEdge(Cell);
 bool isCorner(Cell);
 bool isEmpty(Cell);
 bool isFull(Cell);
 bool countNeighbors(Cell);
-bool setMode();
+bool setMode();*/
 int cornerCount();
 int edgeCount();
 int getRows();
