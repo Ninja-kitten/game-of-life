0688ae7869aae57f6d1a8cb07e84380ffe07a43b
Ninja-kitten <tran260@mail.chapman.edu>
2016-09-20 20:18:38 -0700

    Update GameofLife.h

--- GameofLife.h
+++ GameofLife.h
@@ -1,13 +1,13 @@
 #include "GameBoard.h"
 #include "BoundaryMode.h"
-#include "PauseMode.h"
+#include "DisplayMode.h"
 #include <string>
 class GameofLife{
 public:
-GameofLife(BoundaryMode,PauseMode, int, int);
+GameofLife(int bound,int disp, int r, int c, float d);
 //Constructor for random configuration
 
-GameofLife(BoundaryMode,PauseMode, std::string);
+GameofLife(int bound,int disp, std::string file);
 //constructor for uploaded map with std::string type argument for filename
 
 ~GameofLife();
@@ -15,10 +15,25 @@ GameofLife(BoundaryMode,PauseMode, std::string);
 void nextGen();
 //Moves the gameboard over to the next generation
 
+void mirrorFill();
+//fills the buffer region with X for mirror mode upon creation of the board
+
+void classicFill();
+//fills the buffer region with - for the mirror upon creation of the Board
+
+void doughnutFill();
+//fills the buffer zone at the end of every generation.
+
+void copyBoard();
+//copies a board to make it another one.
+
+void randomFill(float d);
+//
+
 private:
-BoundaryMode boundary();
+BoundaryMode boundary;
 //chooses the boundary taurus 
-pausemode pause();
+DisplayMode display;
 //
 GameBoard oldBoard();
 //Gameboard before the generation change
