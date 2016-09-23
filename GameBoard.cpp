29024733693e45781e491610266f2b4c65e89cbe
sakthikasthuri <kasth100@mail.chapman.edu>
2016-09-22 20:42:44 -0700

    Added some comments yo

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 using namespace std;
 
-GameBoard::GameBoard(){}
-GameBoard::GameBoard(int r, int c){
+GameBoard::GameBoard(){}//default constructor
+GameBoard::GameBoard(int r, int c){//overloaded constructor to get the gameboard size. We make the board one row and column bigger than
+                                  //specified for some clever purposes.
   rows = r;
   columns = c;
   board = new char* [rows+2];
@@ -11,7 +12,7 @@ GameBoard::GameBoard(int r, int c){
     board[i] = new char [columns+2];
   }
 }
-void GameBoard::setParam(int r, int c){
+void GameBoard::setParam(int r, int c){//A function that acts a "reconstructor". Idea came from stack exchange.
   rows = r;
   columns = c;
   board = new char* [rows+2];
@@ -19,14 +20,14 @@ void GameBoard::setParam(int r, int c){
     board[i] = new char [columns+2];
   }
 }
-GameBoard::~GameBoard(){
+GameBoard::~GameBoard(){//destructor
   //destroy
   delete board;
 }
 
-int GameBoard::countNeighbors(int i, int j){
+int GameBoard::countNeighbors(int i, int j){//Function to count the live neighbors around the cell that is specified
     int count;
-    for(int k=i-1; k<=i+1; ++k)
+    for(int k=i-1; k<=i+1; ++k)//nested forloop to check the surrounding 8 neighbors
      {
       for(int l=j-1; l<=j+1; ++l)
           {
@@ -36,22 +37,22 @@ int GameBoard::countNeighbors(int i, int j){
       }
 }
 
-int GameBoard::getRows(){
+int GameBoard::getRows(){//gets the number of rows
   return rows;
   //want this functionality. need to refine
 }
 
-int GameBoard::getColumns(){
+int GameBoard::getColumns(){//gets the number of columns
     return columns;
 }
-char GameBoard::getCell(int i, int j){
+char GameBoard::getCell(int i, int j){//gets the value in the specified cell
   return board[i][j];
 }
-void GameBoard::setCell(int i, int j, char k){
+void GameBoard::setCell(int i, int j, char k){//sets the value of the specified sell to the character specified by k.
   board[i][j] = k;
 }
 
-bool GameBoard::isEqual(GameBoard g){
+bool GameBoard::isEqual(GameBoard g){//checks if the gameboards are equal in size and character
   if(rows != g.getRows()|| columns != g.getColumns()){
     for(int i = 0; i<1000; ++i){
       cout<<"YOU FUCKING IDIOT!!! THEY'RE NOT EVEN THE SAME SIZE"<<endl;
@@ -70,7 +71,7 @@ bool GameBoard::isEqual(GameBoard g){
   return true;
 }
 
-bool GameBoard::isEmpty()
+bool GameBoard::isEmpty()//Checking if the specified cell is empty
 {
   char c = '-';
   for(int i = 1; i<rows+1;++i){
