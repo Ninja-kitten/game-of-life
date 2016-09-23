58dfc67b05665a89f6db7f612c0f9624da6cf2b9
Ninja-kitten <tran260@mail.chapman.edu>
2016-09-22 22:10:46 -0700

    Update GameBoard.cpp

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -22,6 +22,9 @@ void GameBoard::setParam(int r, int c){//A function that acts a "reconstructor".
 }
 GameBoard::~GameBoard(){//destructor
   //destroy
+  for (int i = 0 i<rows+2;++i){
+    delete[] board[i];
+  }
   delete board;
 }
 
