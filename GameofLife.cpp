1cc44b0cec089c28f9cd5b79b59472831238ee17
Ninja-kitten <tran260@mail.chapman.edu>
2016-09-24 15:36:14 -0700

    Update GameofLife.cpp

--- GameofLife.cpp
+++ GameofLife.cpp
@@ -165,10 +165,8 @@ void GameofLife::playGame(){
 		      	break;
 		      
 		      case 3:
-			ofstream fileoutput;
-			fileoutput.open("kasthuritran.out", std::ofstream::app);
-			fileoutput<<"Generation #:\t"<<generation<<endl;
-		      	appendFile();
+			
+		      	appendFile(generation);
 		      //append to file.
 		      //whoever draws a short straw....
 		      	if(generation > 1000)
@@ -284,9 +282,10 @@ string GameofLife::genString(int r){
 	}
 	return str;
 }
-void GameofLife::appendFile(){
+void GameofLife::appendFile(int generation){
 	ofstream fileoutput;
 	fileoutput.open("kasthuritran.out", std::ofstream::app);
+	fileoutput<<"Generation #:\t"<<generation<<endl;
 	for(int i=1; i<row+1; ++i){
 		for(int j = 1; j<column+1;++j){
 			fileoutput<<oldBoard.getCell(i,j);
