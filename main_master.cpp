ddf1b0ccd1c7bd66ae34a531e965414588352b0d
Ninja-kitten <tran260@mail.chapman.edu>
2016-09-24 15:12:49 -0700

    Update main_master.cpp
    got rid of the STOI conversion. added while loops to handle exceptions

--- main_master.cpp
+++ main_master.cpp
@@ -13,66 +13,82 @@ cout << "Would you like to start by providing a (f)ile to run the simulation or 
 cout << "Please enter 'file' or 'random' to start the setup." << endl;
 string answer;
 cin >> answer;
+while(answer != "file"&& answer != "f" && answer != "F" && answer != "random"&& answer != "r"&& answer != "R"){
+  cout<< "Invalid argument! Please try again!"<<endl;
+  cout << "Would you like to start by providing a (f)ile to run the simulation or would you like a (r)andom assignment?"<< endl;
+  cout << "Please enter 'file' or 'random' to start the setup." << endl;
+  cin >> answer;
+}
 if(answer == "file"||answer == "f"||answer == "F")
 {
   
   cout << "To determine the type of boundary, please enter: (1) for Classic, (2) for Doughnut or (3) for Mirror."<< endl;
-  string strboundary;
-  cin >> strboundary;
-  while(strboundary != "1" || strboundary != "2" || strboundary != "3"){
+  int boundary;
+  cin >> boundary;
+  while(boundary != 1 || boundary != 2 || boundary != 3){
     cout << "Invalid argument \n To determine the type of boundary, please enter: (1) for Classic, (2) for Doughnut or (3) for Mirror."<< endl;  
-    cin >> strboundary;
+    cin >> boundary;
   }
-  int boundary = stoi(strboundary,nullptr);
   
   cout<< "To determine the type of display, please enter: (1) for Pause, (2) for Enter or (3) for File." << endl;
-  string strMode;
-  cin >> strMode;
-  while(strMode != "1" || strMode != "2" || strMode != "3"){
+  int mode;
+  cin >> mode;
+  while(mode != "1" || mode != "2" || mode != "3"){
     cout << "Invalid argument \n To determine the type of display, please enter: (1) for Pause, (2) for Enter or (3) for File." << endl;
-    cin >> strMode
+    cin >> mode;
   }
-  int mode = stoi(strMode,nullptr);
   
   string filename;
   cout << "Please enter the name of the file: " << endl;
   cin >> filename;
-  if((boundary == 1 || boundary == 2 || boundary == 3) && (mode == 1 || mode == 2 || mode == 3)){
-    GameofLife gL(boundary,mode, filename);}
-  else{ throw invalid_argument("Please enter the specified integers");}
+  GameofLife gL(boundary,mode, filename);
+  gL.playGame()
 }
 else if(answer == "random"||answer == "r"||answer == "R")
 {
   cout << "Please enter the number of rows you'd like in your grid: "<<endl;
-  string strnumRow;
-  cin >> strnumRow;
-  int numRow = stoi(strnumRow,nullptr);
+  int numRow;
+  cin >> numRow;
+  while(numRow>100|| numRow == 0){
+    cout<<"Tha'ts too big! Please enter a number between 1 and 100"<<endl;
+    cin>> numRow;
+  }
   
   cout << "Please enter the number of columns you'd like in your grid: "<<endl;
-  string strnumCol;
-  cin >> strnumCol;
-  int numCol = stoi(strnumCol,nullptr);
+  int numCol;
+  cin >> numCol;  
+  while(numCol >100 || numCol == 0){
+    cout<<"Tha'ts too big! Please enter a number between 1 and 100"<<endl;
+    cin>> numCol;
+  }
   
   cout << "Please enter a number and number between 0 and 1 to represent the density of filled spaces in your grid: "<< endl;
-  string strdensity;
-  cin >> strdensity;
-  float density = stof(strdensity,nullptr);
-  
-  cout << "To determine the type of boundary, please enter: 1 for Classic, 2 for Doughnut or 3 for Mirror."<< endl;
-  string strboundary;
-  cin >> strboundary;
-  int boundary = stoi(strboundary,nullptr);
-  
-  cout<< "To determine the type of display, please enter: 1 for Pause, 2 for Enter or 3 for File." << endl;
-  string strMode;
-  cin >> strMode;
-  int mode = stoi(strMode,nullptr);
-  
-  if((boundary == 1 || boundary == 2 || boundary == 3) && (mode == 1 || mode == 2 || mode == 3)){
-    GameofLife gL(boundary,mode,numRow,numCol,density);
-    gL.printBoard();
+  float density;
+  cin >> density;
+  while(density<=0||density>=1)
+  {
+    cout<<" INVALID ARGUMENT!!! Please enter a number between the values 0 and 1."<<endl;
+    cin >> density;
   }
-  else{ throw invalid_argument("Please enter the specified integers");}
+  
+  cout << "To determine the type of boundary, please enter: (1) for Classic, (2) for Doughnut or (3) for Mirror."<< endl;
+  int boundary;
+  cin >> boundary;
+  while(boundary != 1 || boundary != 2 || boundary != 3){
+    cout << "Invalid argument \n To determine the type of boundary, please enter: (1) for Classic, (2) for Doughnut or (3) for Mirror."<< endl;  
+    cin >> boundary;
+  }
+  
+  cout<< "To determine the type of display, please enter: (1) for Pause, (2) for Enter or (3) for File." << endl;
+  int mode;
+  cin >> mode;
+  while(mode != "1" || mode != "2" || mode != "3"){
+    cout << "Invalid argument \n To determine the type of display, please enter: (1) for Pause, (2) for Enter or (3) for File." << endl;
+    cin >> mode;
+  }
+  
+    GameofLife gL(boundary,mode,numRow,numCol,density);
+    gL.playGame()
 } 
 else{
  throw invalid_argument("Please enter file or random"); 
