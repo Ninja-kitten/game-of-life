e6b59b9e9cf844d4173b9f7fb70eaf2cd16c43ef
Ninja-kitten <tran260@mail.chapman.edu>
2016-09-24 21:16:20 -0700

    Update main_master.cpp

--- main_master.cpp
+++ main_master.cpp
@@ -6,13 +6,19 @@
 using namespace std;
 
 int main(int argc, char** argv){
-
-
+/*
+Main function:
+1. Prompt for user input to select File input, Random, or Preset Mode.
+2. take in parameters to build game features.
+3. executes geme
+*/
+//takes in user input
 cout << "Welcome to the Game of Life!" << endl;
 cout << "Would you like to start by providing a (f)ile or a (p)reset to run the simulation or would you like a (r)andom assignment?"<< endl;
 cout << "Please enter 'file', 'random' or 'preset' to start the setup." << endl;
 string answer;
 cin >> answer;
+ //checks to see input is valid
 while(answer != "file"&& answer != "f" && answer != "F" && answer != "random"&& answer != "r"&& answer != "R" && answer != "P" && answer != "preset"&& answer != "p"){
   cout<< "Invalid argument! Please try again!"<<endl;
   cout << "Would you like to start by providing a (f)ile to run the simulation or would you like a (r)andom assignment?"<< endl;
@@ -21,10 +27,10 @@ while(answer != "file"&& answer != "f" && answer != "F" && answer != "random"&& 
 }
 if(answer == "file"||answer == "f"||answer == "F")
 {
-  
   cout << "To determine the type of boundary, please enter: (1) for Classic, (2) for Doughnut or (3) for Mirror."<< endl;
   int boundary;
   cin >> boundary;
+ //checks to see if input is valid
   while(boundary != 1 && boundary != 2 && boundary != 3){
     cout << "Invalid argument \n To determine the type of boundary, please enter: (1) for Classic, (2) for Doughnut or (3) for Mirror."<< endl;  
     cin >> boundary;
@@ -33,6 +39,7 @@ if(answer == "file"||answer == "f"||answer == "F")
   cout<< "To determine the type of display, please enter: (1) for Pause, (2) for Enter or (3) for File." << endl;
   int mode;
   cin >> mode;
+   //checks to see if input is valid
   while(mode != 1 && mode != 2 && mode != 3){
     cout << "Invalid argument \n To determine the type of display, please enter: (1) for Pause, (2) for Enter or (3) for File." << endl;
     cin >> mode;
@@ -49,6 +56,7 @@ else if(answer == "random"||answer == "r"||answer == "R")
   cout << "Please enter the number of rows you'd like in your grid: "<<endl;
   int numRow;
   cin >> numRow;
+   //checks to see if input is valid
   while(numRow>100|| numRow == 0){
     cout<<"Tha'ts too big! Please enter a number between 1 and 100"<<endl;
     cin>> numRow;
@@ -57,6 +65,7 @@ else if(answer == "random"||answer == "r"||answer == "R")
   cout << "Please enter the number of columns you'd like in your grid: "<<endl;
   int numCol;
   cin >> numCol;  
+   //checks to see if input is valid
   while(numCol >100 || numCol == 0){
     cout<<"Tha'ts too big! Please enter a number between 1 and 100"<<endl;
     cin>> numCol;
@@ -65,6 +74,7 @@ else if(answer == "random"||answer == "r"||answer == "R")
   cout << "Please enter a number and number between 0 and 1 to represent the density of filled spaces in your grid: "<< endl;
   float density;
   cin >> density;
+   //checks to see if input is valid
   while(density<=0||density>=1)
   {
     cout<<" INVALID ARGUMENT!!! Please enter a number between the values 0 and 1."<<endl;
@@ -74,6 +84,7 @@ else if(answer == "random"||answer == "r"||answer == "R")
   cout << "To determine the type of boundary, please enter: (1) for Classic, (2) for Doughnut or (3) for Mirror."<< endl;
   int boundary;
   cin >> boundary;
+   //checks to see if input is valid
   while(boundary != 1 && boundary != 2 && boundary != 3){
     cout << "Invalid argument \n To determine the type of boundary, please enter: (1) for Classic, (2) for Doughnut or (3) for Mirror."<< endl;  
     cin >> boundary;
@@ -82,6 +93,7 @@ else if(answer == "random"||answer == "r"||answer == "R")
   cout<< "To determine the type of display, please enter: (1) for Pause, (2) for Enter or (3) for File." << endl;
   int mode;
   cin >> mode;
+   //checks to see if input is valid
   while(mode != 1 && mode != 2 && mode != 3){
     cout << "Invalid argument \n To determine the type of display, please enter: (1) for Pause, (2) for Enter or (3) for File." << endl;
     cin >> mode;
@@ -95,6 +107,7 @@ else if(answer == "preset" || answer == "p" || answer == "P")
   cout<< "To determine the type of display, please enter: (1) for Pause, (2) for Enter or (3) for File." << endl;
   int mode;
   cin >> mode;
+   //checks to see if input is valid
   while(mode != 1 && mode != 2 && mode != 3){
     cout << "Invalid argument \n To determine the type of display, please enter: (1) for Pause, (2) for Enter or (3) for File." << endl;
     cin >> mode;
@@ -104,10 +117,12 @@ else if(answer == "preset" || answer == "p" || answer == "P")
   cout<<"In doughnut mode: (5) for Spaceship." <<endl; 
  int presetNum; 
   cin >> presetNum;
+   //checks to see if input is valid
    while(presetNum != 1 && presetNum != 2 && presetNum != 3 && presetNum != 4 && presetNum != 5){
     cout << "Invalid argument \n To determine the type of display, please enter: \n (1) for Small exploder, (2) for Tumbler, (3) for Exploder, (4) 10-cell row or (5) spaceship" << endl;
     cin >> presetNum;
   }
+  //these are all of the presets.
   if(presetNum == 1){
       GameofLife gL(1,mode,"Preset_smallExploder.txt");
       gL.playGame();
